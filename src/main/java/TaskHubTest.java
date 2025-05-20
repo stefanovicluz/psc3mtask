990be204c56ce8b6d34d06a4d3df985cca9d27fb
stefanovicluz <joaovitorluz135060@gmail.com>
2025-05-20 11:40:52 -0300

    feat: teste de conexão e manipulão do SQLite

---
+++ src/main/java/TaskHubTest.java
@@ -0,0 +1,37 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import models.Usuario;
+
+public class TaskHubTest {
+    private static EntityManagerFactory emf;
+    public static void main(String[] args) {
+        emf = Persistence.createEntityManagerFactory(
+                "PU"
+        );
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            Usuario usuario1 =
+                    new Usuario(1,
+                            "Dick Vigarista",
+                            "dickvigarista@gmail.com"
+                            ,"dick123");
+            Usuario usuario2 =
+                    new Usuario(2,
+                            "MUttley",
+                            "muttley@gamil.com"
+                                    ,"muttley123");
+
+          //Persistir Usuários
+            em.getTransaction().begin();
+            em.persist(usuario1);
+            em.persist(usuario2);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+            emf.close();
+        }
+
+    }
+}
